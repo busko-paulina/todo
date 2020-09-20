67388857db3352b937f7bb23100c4fa279a4fcd2
Poli <busk.paulina@gmail.com>
2020-09-20 15:43:20 +0200

    added

---
+++ assets/js/todo.js
@@ -0,0 +1,24 @@
+// chceck off todo by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//click on x to delete todo
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e){
+	if(e.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
